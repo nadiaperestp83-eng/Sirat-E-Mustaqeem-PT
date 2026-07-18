a9ed91aad291fe089adf8ef7599727b4c7f437ad
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-07-18 03:11:00 -0300

    Update date_controller.dart

--- lib/src/core/util/controller/date_controller.dart
+++ lib/src/core/util/controller/date_controller.dart
@@ -2,8 +2,11 @@ import 'package:hijri_calendar/hijri_calendar.dart';
 import 'package:intl/intl.dart';
 
 /// function to get today's date
-String getTodayDate() {
-  return DateFormat.yMMMMd().format(
+/// [localeName] segue o idioma atual do app (ex: 'en', 'pt') para exibir
+/// o mês e demais textos no idioma correto. Se omitido, usa o locale
+/// padrão do dispositivo/pacote intl.
+String getTodayDate({String? localeName}) {
+  return DateFormat.yMMMMd(localeName).format(
     DateTime.now(),
   );
 }
