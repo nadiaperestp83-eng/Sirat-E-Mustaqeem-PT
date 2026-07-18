760ae69dbef2974d157632c53a05dbff0578578d
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-07-18 02:57:36 -0300

    Update collection.dart

--- lib/src/features/home/model/collection.dart
+++ lib/src/features/home/model/collection.dart
@@ -1,62 +1,79 @@
 import '../../../../routes/routes.dart';
 
+/// Identifica qual texto traduzido (AppLocalizations) deve ser exibido
+/// como título de cada item da grade "Collection" na Home.
+/// A resolução da chave -> texto localizado acontece em CollectionButton,
+/// que tem acesso ao BuildContext.
+enum CollectionTitleKey {
+  quran,
+  hadees,
+  dua,
+  tasbih,
+  azkars,
+  allahNames,
+  prayerTimes,
+  qiblaDirection,
+  liveTv,
+  others,
+}
+
 class Collection {
   final String assetName;
-  final String title;
+  final CollectionTitleKey titleKey;
   final String routeName;
 
-  Collection(this.assetName, this.title, this.routeName);
+  Collection(this.assetName, this.titleKey, this.routeName);
 }
 
 List<Collection> collections = [
   Collection(
     'assets/images/collection_icon/svg/quran.svg',
-    'Quran',
+    CollectionTitleKey.quran,
     RouteGenerator.quran,
   ),
   Collection(
     'assets/images/collection_icon/svg/hadees.svg',
-    'Hadees',
+    CollectionTitleKey.hadees,
     'Coming Soon',
   ),
   Collection(
     'assets/images/collection_icon/svg/duas.svg',
-    'Dua',
+    CollectionTitleKey.dua,
     RouteGenerator.dua,
   ),
   Collection(
     'assets/images/collection_icon/svg/tasbih.svg',
-    'Tasbih',
+    CollectionTitleKey.tasbih,
     RouteGenerator.tasbih,
   ),
   Collection(
     'assets/images/collection_icon/svg/other.svg',
-    'Azkars',
+    CollectionTitleKey.azkars,
     RouteGenerator.azkar,
   ),
   Collection(
     'assets/images/collection_icon/svg/allah.svg',
-    '99 Names of Allah',
+    CollectionTitleKey.allahNames,
     RouteGenerator.allahName,
   ),
   Collection(
     'assets/images/collection_icon/svg/prayer_time.svg',
-    'Prayer Times',
+    CollectionTitleKey.prayerTimes,
     RouteGenerator.prayerTimingPage,
   ),
   Collection(
     'assets/images/collection_icon/svg/kiblat.svg',
-    'Qabah Direction',
+    CollectionTitleKey.qiblaDirection,
     RouteGenerator.qibla,
   ),
   Collection(
     'assets/images/collection_icon/svg/qaabah.svg',
-    'Live TV',
+    CollectionTitleKey.liveTv,
     RouteGenerator.liveTv,
   ),
   Collection(
     'assets/images/collection_icon/svg/other.svg',
-    'Others',
+    CollectionTitleKey.others,
     'Coming Soon',
   ),
 ];
